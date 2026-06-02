9be2f2b1d67872bf5afcb4c1e4d85cb150671662
Srijan Ray <offsray7044@gmail.com>
2026-06-02 17:36:43 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,7 +8,7 @@ import Navbar from './Components/Navbar'
 import Builder from './pages/Builder'
 import Billing from './pages/Billing'
 import { Toaster } from "react-hot-toast"
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://delaiserver.onrender.com"
 export const CLIENT_URL = "http://localhost:5173"
 function App() {
   const [user, setUser] = useState(null)
